67a0bb0d079297c3aa177a8ab99a15e5f0ebcceb
liulixiang1988 <liulixiang1988@gmail.com>
2016-05-01 21:45:34 +0800

    数据仓库

--- giflib/src/main/java/io/github/liulixiang1988/controller/GifController.java
+++ giflib/src/main/java/io/github/liulixiang1988/controller/GifController.java
@@ -1,15 +1,20 @@
 package io.github.liulixiang1988.controller;
 
+import io.github.liulixiang1988.data.GifRepository;
 import io.github.liulixiang1988.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDate;
 
 @Controller //1. 标识控制器
 public class GifController {
+
+    @Autowired //依赖注入
+    private GifRepository gifRepository;
+
     @RequestMapping("/") //2. 添加映射
     public String listGifs() {
         return "home"; //3. 返回模板名称
@@ -17,7 +22,7 @@ public class GifController {
 
     @RequestMapping("/gif")
     public String getDetails(ModelMap modelMap) {
-        Gif gif = new Gif("android-explosion", LocalDate.of(2016, 5, 1), "刘理想", true);
+        Gif gif = gifRepository.findByName("ben-and-mike");
         modelMap.put("gif", gif);
         return "gif-details";
     }
---
+++ giflib/src/main/java/io/github/liulixiang1988/data/GifRepository.java
@@ -0,0 +1,29 @@
+package io.github.liulixiang1988.data;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+import io.github.liulixiang1988.model.Gif;
+import org.springframework.stereotype.Component;
+
+@Component //声明组件,用于依赖注入
+public class GifRepository {
+    private static final List<Gif> ALL_GIFS = Arrays.asList(
+            new Gif("android-explosion", LocalDate.of(2016, 5, 1), "刘理想", true),
+            new Gif("ben-and-mike", LocalDate.of(2016, 4, 10), "刘理想", true),
+            new Gif("book-dominos", LocalDate.of(2016, 4, 1), "刘理想", true),
+            new Gif("complier-bot", LocalDate.of(2016, 4, 12), "刘理想", true),
+            new Gif("cowboy-coder", LocalDate.of(2016, 4, 13), "刘理想", false),
+            new Gif("infinite-andrew", LocalDate.of(2016, 4, 15), "刘理想", true)
+    );
+
+    public Gif findByName(String name) {
+        for (Gif gif : ALL_GIFS) {
+            if (gif.getName().equals(name)) {
+                return gif;
+            }
+        }
+        return null;
+    }
+}
